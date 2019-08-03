6fa9a0415485a65131d79998c164b1133add1c9e
Dan Ho <igaryhe@gmail.com>
2019-08-03 13:15:26 +0800

    2ddl imported

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _speed = 2f;
+    private void Update()
+    {
+        LookAtMousePosition();
+        Move();
+    }
+
+    private void LookAtMousePosition()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var direction = (Vector2) (mousePos - transform.position).normalized;
+        transform.up = direction;
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            var deltaY = _speed * Time.deltaTime;
+            transform.position += new Vector3(0, deltaY);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            var deltaY = _speed * Time.deltaTime;
+            transform.position += new Vector3(0, -deltaY);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            var deltaX = _speed * Time.deltaTime;
+            transform.position += new Vector3(-deltaX, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            var deltaX = _speed * Time.deltaTime;
+            transform.position += new Vector3(deltaX, 0);
+        }
+    }
+}
